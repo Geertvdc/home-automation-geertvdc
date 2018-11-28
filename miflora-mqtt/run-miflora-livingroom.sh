7ad7b9e5e83aacba9656b2da426723f70e0cae96
Geert van der Cruijsen <geert@vdcruijsen.net>
2018-11-28 22:36:06 +0100

    miflora check once per hour

--- miflora-mqtt/run-miflora-livingroom.sh
+++ miflora-mqtt/run-miflora-livingroom.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-docker run -d --net=host --volume ./config.livingroom.ini:/miflora-mqtt-daemon/config.ini --name miflora --restart always geertvdc/miflora-mqtt
+docker run -d --net=host --volume /tmp/miflora/config.livingroom.ini:/miflora-mqtt-daemon/config.ini --name miflora --restart always geertvdc/miflora-mqtt

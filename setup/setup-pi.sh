6f6b318f2f9ee1dc4226a6b8bd2549dc2119cd1c
Geert van der Cruijsen <geert@vdcruijsen.net>
2019-09-02 19:55:31 +0200

    updated setup script

--- setup/setup-pi.sh
+++ setup/setup-pi.sh
@@ -2,21 +2,47 @@
 #next steps manual:
 
 #install docker
-curl -ssl https://get.docker.com | sh
+# Install some required packages first
+sudo apt update
+sudo apt install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
 
-sudo usermod -a -G docker $USER
-sudo gpasswd -a $USER docker
+# Get the Docker signing key for packages
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
 
-newgrp docker
+# Add the Docker official repos
+echo "deb [arch=armhf] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+     $(lsb_release -cs) stable" | \
+    sudo tee /etc/apt/sources.list.d/docker.list
 
-#install docker-compose
-sudo apt-get -y install python-pip
-
-sudo pip install docker-compose
+# Install Docker
+# The aufs package, part of the "recommended" packages, won't install on Buster just yet, because of missing pre-compiled kernel modules.
+# We can work around that issue by using "--no-install-recommends"
+sudo apt update
+sudo apt install -y --no-install-recommends \
+    docker-ce \
+    cgroupfs-mount
 
 #setup docker-compose network
 
+sudo systemctl enable docker
+sudo systemctl start docker
+
 docker network create traefik_proxy
+
+#docker-compose 
+
+# Install required packages
+sudo apt update
+sudo apt install -y python python-pip libffi-dev python-backports.ssl-match-hostname
+
+# Install Docker Compose from pip
+# This might take a while
+sudo pip install docker-compose
 
 #install git 
 sudo apt-get update
@@ -25,9 +51,9 @@ sudo apt-get install git
 
 mkdir azure-devops-agent
 
-curl https://vstsagentpackage.azureedge.net/agent/2.144.0/vsts-agent-linux-arm-2.144.0.tar.gz --output vsts-agent-linux-arm-2.144.0.tar.gz
+curl https://vstsagentpackage.azureedge.net/agent/2.156.0/vsts-agent-linux-arm-2.156.0.tar.gz --output vsts-agent-linux-arm-2.156.0.tar.gz
 
-tar zxvf vsts-agent-linux-arm-2.144.0.tar.gz
+tar zxvf vsts-agent-linux-arm-2.156.0.tar.gz
 
 #on non portainer pi set the docker daemon to be available over tcp so it can be controlled by portainer.
 #create a file in /etc/systemd/system/docker.service.d/override.conf
